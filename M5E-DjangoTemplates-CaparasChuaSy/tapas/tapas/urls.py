2ed1cbb5bd0f059bbe7ffb40af9dc7d9331a3c6f
Calista <calista.caparas@gmail.com>
2026-03-10 11:38:37 +0800

    .

--- M5E-DjangoTemplates-CaparasChuaSy/tapas/tapas/urls.py
+++ M5E-DjangoTemplates-CaparasChuaSy/tapas/tapas/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tapasapp.urls')),
-    path('add_menu', views.add_menu, name='add_menu'),
 ]
